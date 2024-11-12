06c2c4930a643e076324a7c74ab43e1cc45aa11f
Kalil Pereira <kalil.alves.2019@gmail.com>
2024-11-12 20:20:05 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿using DesafioPOO.Models;
+using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456789", "Nokia X", "111111111111111", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        Smartphone iphone = new Iphone("987654321", "iPhone 12", "222222222222222", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
